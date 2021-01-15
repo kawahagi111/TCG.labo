675f0c888938e764fa10e7c40a5f5855f0d58240
kawahagi111 <0tg0cw0b78v3x8t@au.com>
2021-01-15 16:46:23 +0900

    Active Storageの導入

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,21 @@
 class TweetsController < ApplicationController
 
     def index
+      @tweet = Tweet.all
     end
+
+    def create
+      @tweet = Tweet.new(tweet_params)
+    end
+
+    def new
+    end
+
+    private
+
+    def tweet_params
+      params.require(:tweet).permit(:title, :image, :concept).merge(user_id: current_user.id)
+    end
+
 
 end
